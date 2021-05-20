7602cbe230e17e88552250dfdb7389df7ae9330d
Rekha Remadevi <50706192+rekharchandran@users.noreply.github.com>
2021-05-20 21:00:44 +0200

    Add files via upload

---
+++ First Steps in SQL/Creating Table and Databases/SQLQuery1.sql
@@ -0,0 +1,24 @@
+Create Table Employee
+(
+ EmployeeId int
+ ,EmployeeFirstName varchar(100)
+ , EmployeeLastName varchar(100)
+ ,Salary int
+ ,[Address] varchar(500)
+)
+
+Go
+
+Create Schema Food
+
+Create Table Food.Employee
+(
+     EmployeeId int
+    ,EmployeeFirstName varchar(100)
+    , EmployeeLastName varchar(100)
+    ,Salary int
+     ,[Address] varchar(500)
+
+ )
+
+ Go
